36d1958948c369a734481a89d0fb65ba26cb5933
SzymonRem <126695909+SzymonRem@users.noreply.github.com>
2023-03-15 15:20:57 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    int i=0;
+    int suma=0;
+    for(;i<=100;i++){
+      if(i%2==0){
+        suma+=i;
+      }
+    }
+      System.out.println(suma);
+  }
+}
